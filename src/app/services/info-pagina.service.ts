929fd33fbc60bc3cbf469cdb572b1d5cbbafc154
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2018-12-01 16:37:32 -0300

    interface para info de pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../intefaces/info-pagina.inteface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
+  info: InfoPagina = {};
+  cargada = false;
 
   constructor(private http: HttpClient) {
+
     this.http.get('assets/data/data-pagina.json')
-    .subscribe( resp => {
-      console.log(resp);
+    .subscribe( (resp: InfoPagina) => {
+      console.log(resp.email);
+      this.cargada = true;
+      this.info = resp;
     });
   }
 }
